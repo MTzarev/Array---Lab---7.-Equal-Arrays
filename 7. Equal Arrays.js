1556aecb657da8a192cbe5ae6fa1743d3ff50d1d
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-29 17:12:26 +0200

    Add files via upload

---
+++ 7. Equal Arrays.js
@@ -0,0 +1,16 @@
+function equalArrays(array1, array2) {
+    let sum = 0;
+    //let isIdentical = true;
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            // isIdentical=true;
+            console.log(`Arrays are not identical. Found difference at ${i} index`);
+            return;
+        }
+        sum += Number(array1[i]);
+    }
+    console.log(`Arrays are identical. Sum: ${sum}`)
+}
+//equalArrays(['10', '20', '30'], ['10', '20', '30']);
+//equalArrays(['1','2','3','4','5'], ['1','2','4','4','5']);
+equalArrays(['1'], ['10']);
